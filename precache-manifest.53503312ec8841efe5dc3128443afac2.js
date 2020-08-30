67954988cebf88bd6dbb08596ff790d66725eef3
raphael.torres <raphael.torres@softplan.com.br>
2020-08-30 00:56:34 -0300

    Updates

---
+++ precache-manifest.53503312ec8841efe5dc3128443afac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1ac97fb778a96c1a91b55b31fffe38",
+    "url": "/memorias/index.html"
+  },
+  {
+    "revision": "06f1f4ff306eba0dbf2b",
+    "url": "/memorias/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "06f1f4ff306eba0dbf2b",
+    "url": "/memorias/static/js/2.ab3319c2.chunk.js"
+  },
+  {
+    "revision": "b3924902b6517db7f2b56facf8ba237b",
+    "url": "/memorias/static/js/2.ab3319c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3125f4abfb5ef1a83e1",
+    "url": "/memorias/static/js/main.3f41a5b1.chunk.js"
+  },
+  {
+    "revision": "6d183db97703e9838c97",
+    "url": "/memorias/static/js/runtime-main.8a832396.js"
+  }
+]);

8ff6197b0cf50fe70aa451f9e9218b33724cdeb0
raphael.torres <raphael.torres@softplan.com.br>
2020-08-30 03:14:15 -0300

    Updates

---
+++ precache-manifest.22b184bddbdfcb2c33464a4602110234.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57621a1aecbdb30f92ceb157a39f616",
+    "url": "/memorias/index.html"
+  },
+  {
+    "revision": "26c364d259fa6941638e",
+    "url": "/memorias/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "e740c6ea16d6c88bdd0d",
+    "url": "/memorias/static/css/main.0f501cc9.chunk.css"
+  },
+  {
+    "revision": "26c364d259fa6941638e",
+    "url": "/memorias/static/js/2.03b833b6.chunk.js"
+  },
+  {
+    "revision": "b3924902b6517db7f2b56facf8ba237b",
+    "url": "/memorias/static/js/2.03b833b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e740c6ea16d6c88bdd0d",
+    "url": "/memorias/static/js/main.e4cbddc3.chunk.js"
+  },
+  {
+    "revision": "6d183db97703e9838c97",
+    "url": "/memorias/static/js/runtime-main.8a832396.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cb9a4ac96076e792a6136ede44967300.js"
+  "/memorias/precache-manifest.22b184bddbdfcb2c33464a4602110234.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/memorias/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

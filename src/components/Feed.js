f611026f4078559211d3234e498be62d7d4fbc6b
raphael.torres <raphael.torres@softplan.com.br>
2020-08-30 01:00:32 -0300

    Update image src

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,10 +3,10 @@ import Carousel from './Carousel';
 
 export default function Feed() {
   const carouselImgs = [
-    { src: '/images/1.jpg', alt: 2014 },
-    { src: '/images/2.jpg', alt: 2015 },
-    { src: '/images/3.jpg', alt: 2016 },
-    { src: '/images/4.jpg', alt: 2017 },
+    { src: './images/1.jpg', alt: 2014 },
+    { src: './images/2.jpg', alt: 2015 },
+    { src: './images/3.jpg', alt: 2016 },
+    { src: './images/4.jpg', alt: 2017 },
   ];
 
   return (
